e071b63ab31bc38fc96f3712bc56115874b3b491
Sarah MacDougall <smacdoug@calpoly.edu>
2019-11-09 15:03:50 -0800

    Added first map to grid test

--- Assets/Scripts/GridTests.cs
+++ Assets/Scripts/GridTests.cs
@@ -5,14 +5,48 @@ using UnityEngine;
 public class GridTests : MonoBehaviour
 {
     public MapGrid<GridContainer> grid;
-    public GameObject swamp;
-    public GameObject farmland;
-    public GameObject hill;
+    public GameObject small;
+    public GameObject medium;
+    public GameObject large;
+    public GameObject mediumVillage;
+    public GameObject mediumForest; 
+    public GameObject water; 
     void Start()
     {
         grid = new MapGrid<GridContainer>(10, 10, 1, new Vector3(0,0,0), (MapGrid<GridContainer> g, int x, int y) => new GridContainer(g, x, y));
-        
+
         for (int x = 0; x < grid.gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.gridArray.GetLength(1); y++)
+            {
+                grid.gridArray[x,y].AddUnit(water);
+            }
+        }
+        
+        grid.gridArray[4,8].AddUnit(mediumForest); 
+        grid.gridArray[5,8].AddUnit(medium);
+        grid.gridArray[5,7].AddUnit(medium);
+        grid.gridArray[4,7].AddUnit(medium);
+        grid.gridArray[7,5].AddUnit(mediumVillage);
+        grid.gridArray[2,5].AddUnit(mediumForest);
+        grid.gridArray[8,3].AddUnit(medium);
+        grid.gridArray[3,2].AddUnit(medium);
+        grid.gridArray[2,2].AddUnit(medium);
+
+        grid.gridArray[6,7].AddUnit(large);
+        grid.gridArray[1,5].AddUnit(large);
+        grid.gridArray[1,4].AddUnit(large);
+        grid.gridArray[3,3].AddUnit(large);
+
+        grid.gridArray[3,6].AddUnit(small);
+        grid.gridArray[4,6].AddUnit(small);
+        grid.gridArray[4,3].AddUnit(small);
+        grid.gridArray[4,2].AddUnit(small);
+        grid.gridArray[5,2].AddUnit(small);
+        grid.gridArray[7,3].AddUnit(small);
+
+
+        /*for (int x = 0; x < grid.gridArray.GetLength(0); x++)
         {
             for (int y = 0; y < grid.gridArray.GetLength(1); y++)
             {
@@ -31,6 +65,6 @@ public class GridTests : MonoBehaviour
                     grid.gridArray[x,y].AddUnit(swamp);
                 }
             }
-        }
+        }*/
     }
 }
