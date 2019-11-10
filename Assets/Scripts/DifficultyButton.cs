acac805a93927fabcea5b7ff3a9ad0cd98390325
Sarah MacDougall <smacdoug@calpoly.edu>
2019-11-09 19:01:31 -0800

    initial title screen, game over screen, and in game ui

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    public int difficulty;
+
+    private Button button;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); //MUST be exact name of Game Manager obj in Scene
+
+        button.onClick.AddListener(SetDifficulty); //when our button is clicked, set difficulty
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManager.StartGame(difficulty);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System;
+public class GameManager : MonoBehaviour
+{
+    //UI vars
+    public TextMeshProUGUI bridgesText;
+    public GameObject titleScreen;
+    public GameObject gameOverScreen;
+
+    //public vars
+    public bool isGameActive;
+
+    //private vars
+    private int bridges;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkIfGameOver();
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+
+        bridgesText.gameObject.SetActive(true);
+        bridges = 0;
+        UpdateBridges(0);
+
+        float spawnRate = 1.0f / difficulty;
+        StartCoroutine(Example(spawnRate)); //do you need to do something over time? it needs to be a coroutine
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    #region Update Game (example): gain 1 bridge every 1 or 1/2 or 1/3 sec (difficulty determines this), game over at 10 bridges
+
+    private void checkIfGameOver()
+    {
+        if (bridges == 10) { GameOver(); }
+    }
+
+    void UpdateBridges(int bridgesChange)
+    {
+        if (isGameActive)
+        {
+            bridges += bridgesChange;
+            bridgesText.text = "Bridges: " + bridges;
+        }
+    }
+
+    IEnumerator Example(float spawnRate)
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            UpdateBridges(1);
+            if (!isGameActive) { break; }
+        }
+    }
+
+    #endregion
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverScreen.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
